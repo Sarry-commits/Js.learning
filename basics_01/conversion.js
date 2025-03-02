75ffcb10c82696244fd427a373f873d65af6f139
Sarry-commits <Sarimkhan822640@gmail.com>
2025-03-02 10:28:55 +0000

    till conversion done

---
+++ basics_01/conversion.js
@@ -0,0 +1,14 @@
+let score = null
+console.log (score)
+let valueInNumber = score
+// console.log (typeof valueInNumber);
+
+let IsLoogedIn = 1
+let booleanIsloogedIn = Boolean (IsLoogedIn)
+// console.log (booleanIsloogedIn);
+
+let someNumber = 55
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
---
+++ basics_01/variables_01.js
@@ -0,0 +1,14 @@
+const accountID = 822640
+let accountEmail = "Sarim@gmail.com"
+var accountPassword = "Sarry123"
+accountCity = "Bhopal"
+
+accountEmail = "Sarry@gmail.com"
+accountPassword = "Sarim789"
+accountCity = "BPL"
+
+/*
+Avoid using var because of the block scope issue
+*/
+
+console.table ([accountEmail, accountPassword, accountCity])
